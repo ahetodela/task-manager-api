e7ee715f8cc6d1adbeecb9de9cb93f44a4d96b38
Dela Aheto <dela.aheto@piceasoft.com>
2021-10-04 12:21:24 +0300

    Used env variable in the mongoose connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
+mongoose.connect(process.env.MONGO_DB_URL);
 //the useUrlParser: true, useCreateIndex:true have been deprecated. that's why they are no more provided as options to the connect method on mongoose
